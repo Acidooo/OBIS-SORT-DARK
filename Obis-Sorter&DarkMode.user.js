65b73ff966f3c69ece17e3afe8f0f503c1557243
Acidooo <15827009+Acidooo@users.noreply.github.com>
2020-06-11 21:46:56 +0300

    Update Obis-Sorter&DarkMode.user.js

--- Obis-Sorter&DarkMode.user.js
+++ Obis-Sorter&DarkMode.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Obis:Sorter&DarkMode
-// @version      2.9.2
+// @version      2.9.3
 // @description  try to take over the world!
 // @author       Acido
 // @match        *://*.ktun.edu.tr/Ogrenci/*
